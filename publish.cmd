4728db5e3fd729c2b160e71c68d5203db3520c97
Kaustav Banerjee <banerjee.kaustav@outlook.com>
2019-07-11 17:37:57 +0530

    Add files via upload
    Try 1

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=vodafonekaustavqna-bot-Web-Deploy.pubxml -p:Password=k0BlyawnQAjyyC8XhmQ4pa63XlTzpLTlDh3aD7kcEJJ8kegrTgSWoncCHPWy
+
